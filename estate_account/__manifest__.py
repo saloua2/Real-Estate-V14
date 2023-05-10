a3bd927195bde677188877cf0fe9f879ef170104
salwa-ksila <ksila.salwa@gmail.com>
2023-05-10 16:25:46 +0100

    Report Inheritance

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -10,6 +10,7 @@
     # data files always loaded at installation
     'data': [
         'security/ir.model.access.csv',
+        'report/estate_property_report.xml',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -4,30 +4,32 @@ class InheritedModel(models.Model):
     _inherit = "estate.property"
 
     def action_sold(self):
+        res = super().action_sold()
         print(" reached ".center(100, '='))
-        self.env["account.move"].create(
-            {
-                "name": "Invoice of %s " %self.name,
-                "partner_id": self.env.user.partner_id.id,
-                "move_type": "out_invoice",
-                "invoice_line_ids": [
-                    (0,0,
-                        {
-                            "name": self.name,
-                            "quantity": 1,
-                            "account_id":1,
-                            "price_unit": self.selling_price * 0.6,
-                        },
-                    ),
-                    (0,0,
-                        {
-                            "name": "administrative fees",
-                            "quantity": 1,
-                            "account_id": 1,
-                            "price_unit": 100.00,
-                        },
-                    )
-                ],
-            }
-        )
-        return super().action_sold()
+        for rec in self:
+            self.env["account.move"].create(
+                {
+                    "name": "Invoice of %s " %rec.name,
+                    "partner_id": self.env.user.partner_id.id,
+                    "move_type": "out_invoice",
+                    "invoice_line_ids": [
+                        (0,0,
+                            {
+                                "name": rec.name,
+                                "quantity": 1,
+                                "account_id":1,
+                                "price_unit": rec.selling_price * 0.6,
+                            },
+                        ),
+                        (0,0,
+                            {
+                                "name": "administrative fees",
+                                "quantity": 1,
+                                "account_id": 1,
+                                "price_unit": 100.00,
+                            },
+                        )
+                    ],
+                }
+            )
+            return res
